28127ba243dd00b77336a5de9140d7f53710d062
Iniko Matthew Jemima <109912460+InikoMatthewPro@users.noreply.github.com>
2022-08-18 12:48:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 echo "Update & Upgrade"
 apt update 
-apt upgrade
+apt upgrade -y
+pkg update
+pkg uograde -y
 clear
+echo "Installing & Other"
+pkg i git nodejs wget
+clear
+"Check Version Nodejs & npm"
+npm -v
+"Download Dodo Bot Files Code"
+git clone https://github.com/DodoGames7/Dodo-Bot.git
