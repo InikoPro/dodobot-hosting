c6cfb0c53b0ebb8dc83bf4668dc458738c32457d
Iniko Matthew Jemima <109912460+InikoMatthewPro@users.noreply.github.com>
2022-08-20 20:36:46 +0530

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,1 +1,8 @@
-
+echo "Install git & other"
+apt install git
+apt install wget
+apt install nodejs npm
+echo "Install Dodo bot using github"
+git clone https://github.com/DodoGames7/Dodo-Bot.git
+cd Dodo-Bot
+npm i
