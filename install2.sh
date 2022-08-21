d8931fd7ef0a62b6feb26055d6b6072ec3dfc4ab
Iniko Matthew Jemima <109912460+InikoMatthewPro@users.noreply.github.com>
2022-08-21 10:17:07 +0530

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,7 +1,7 @@
 echo "Install git & other"
-apt install git
-apt install wget
-apt install nodejs npm
+apt install git wget curl -y
+curl -s https://deb.nodesource.com/setup_16.x | sudo bash
+apt install nodejs -y
 echo "Install Dodo bot using github"
 git clone https://github.com/DodoGames7/Dodo-Bot.git
 cd Dodo-Bot
